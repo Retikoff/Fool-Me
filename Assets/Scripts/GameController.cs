a266c209e1c31c5d1c0bed8a211b3337df13568d
Andrei Retsikau <154879381+Retikoff@users.noreply.github.com>
2025-04-15 21:24:34 +0300

    debug features

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -28,13 +29,62 @@ public class GameController : MonoBehaviour
         if(GUI.Button(new Rect(760,100,100,30), "+1")){
 
         }
-
         if(GUI.Button(new Rect(760, 140, 100, 30), "*2")){
 
         }
-
         if(GUI.Button(new Rect(760, 180, 100, 30), "-1")){
 
+        }
+        if(GUI.Button(new Rect(260, 10, 40, 40), "0")){
+
+        }
+        if(GUI.Button(new Rect(310, 10, 40, 40), "1")){
+
+        }
+        if(GUI.Button(new Rect(360, 10, 40, 40), "2")){
+            
+        }
+        if(GUI.Button(new Rect(410, 10, 40, 40), "3")){
+   
+        }
+        if(GUI.Button(new Rect(460, 10, 40, 40), "3")){   
+
+        }
+        if(GUI.Button(new Rect(510, 10, 40, 40), "4")){
+            
+        }
+        if(GUI.Button(new Rect(560, 10, 40, 40), "5")){
+            
+        }
+        if(GUI.Button(new Rect(610, 10, 40, 40), "6")){
+            
+        }
+        if(GUI.Button(new Rect(660, 10, 40, 40), "7")){
+            
+        }
+        if(GUI.Button(new Rect(710, 10, 40, 40), "8")){
+            
+        }
+        if(GUI.Button(new Rect(760, 10, 40, 40), "9")){
+            
+        }
+        if(GUI.Button(new Rect(260, 60, 40, 40), "10")){
+            
+        }
+        if(GUI.Button(new Rect(310, 60, 40, 40), "11")){
+            
+        }
+        if(GUI.Button(new Rect(360, 60, 40, 40), "12")){
+            
+        }
+        if(GUI.Button(new Rect(410, 60, 40, 40), "13")){
+            
+        }
+        if(GUI.Button(new Rect(460, 60, 40, 40), "14")){
+            
+        }
+        if(GUI.Button(new Rect(510, 60, 40, 40), "15")){
+            
         }
     }
 
