b675a3f1e1dce0825b76cca1e0f31ad9ecfd6547
Andrei Retsikau <154879381+Retikoff@users.noreply.github.com>
2025-04-28 19:32:07 +0300

    BoostApply animation

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
@@ -24,70 +26,6 @@ public class GameController : MonoBehaviour
         if( selectedCard != null && (selectedCardValue != selectedCard.GetComponent<CardController>().CardValue)){
             selectedCardValue = selectedCard.GetComponent<CardController>().CardValue; 
         }
-    }
-
-    //debug for init implementation
-    void OnGUI()
-    {
-        #region DEBUG_SETTERS
-        if(GUI.Button(new Rect(760,100,100,30), "+1")){
-            CardFactory.ChangeNumericCard(selectedCard, selectedCard.GetComponent<CardController>().CardValue + 1);
-        }
-        if(GUI.Button(new Rect(760, 140, 100, 30), "*2")){
-            CardFactory.ChangeNumericCard(selectedCard, selectedCard.GetComponent<CardController>().CardValue * 2);
-        }
-        if(GUI.Button(new Rect(760, 180, 100, 30), "-1")){
-            CardFactory.ChangeNumericCard(selectedCard, selectedCard.GetComponent<CardController>().CardValue - 1);
-        }
-        if(GUI.Button(new Rect(260, 10, 40, 40), "0")){
-            CardFactory.ChangeNumericCard(selectedCard, 0);
-        }
-        if(GUI.Button(new Rect(310, 10, 40, 40), "1")){
-            CardFactory.ChangeNumericCard(selectedCard, 1);
-        }
-        if(GUI.Button(new Rect(360, 10, 40, 40), "2")){
-            CardFactory.ChangeNumericCard(selectedCard, 2);
-        }
-        if(GUI.Button(new Rect(410, 10, 40, 40), "3")){
-            CardFactory.ChangeNumericCard(selectedCard, 3);
-        }
-        if(GUI.Button(new Rect(460, 10, 40, 40), "4")){
-            CardFactory.ChangeNumericCard(selectedCard, 4);
-        }
-        if(GUI.Button(new Rect(510, 10, 40, 40), "5")){
-            CardFactory.ChangeNumericCard(selectedCard, 5);
-        }
-        if(GUI.Button(new Rect(560, 10, 40, 40), "6")){
-            CardFactory.ChangeNumericCard(selectedCard, 6);
-        }
-        if(GUI.Button(new Rect(610, 10, 40, 40), "7")){
-            CardFactory.ChangeNumericCard(selectedCard, 7);
-        }
-        if(GUI.Button(new Rect(660, 10, 40, 40), "8")){
-            CardFactory.ChangeNumericCard(selectedCard, 8);
-        }
-        if(GUI.Button(new Rect(710, 10, 40, 40), "9")){
-            CardFactory.ChangeNumericCard(selectedCard, 9);
-        }
-        if(GUI.Button(new Rect(260, 60, 40, 40), "10")){
-            CardFactory.ChangeNumericCard(selectedCard, 10);
-        }
-        if(GUI.Button(new Rect(310, 60, 40, 40), "11")){
-            CardFactory.ChangeNumericCard(selectedCard, 11);
-        }
-        if(GUI.Button(new Rect(360, 60, 40, 40), "12")){
-            CardFactory.ChangeNumericCard(selectedCard, 12);
-        }
-        if(GUI.Button(new Rect(410, 60, 40, 40), "13")){
-            CardFactory.ChangeNumericCard(selectedCard, 13);
-        }
-        if(GUI.Button(new Rect(460, 60, 40, 40), "14")){
-            CardFactory.ChangeNumericCard(selectedCard, 14);
-        }
-        if(GUI.Button(new Rect(510, 60, 40, 40), "15")){
-            CardFactory.ChangeNumericCard(selectedCard, 15);
-        }
-        #endregion 
     }
 
     public void TurnCardSelected(GameObject card){
@@ -120,10 +58,24 @@ public class GameController : MonoBehaviour
         char actionOperation = action[0];
         char actionValue = action[1];
 
-        obj.transform.DOLocalMove(selectedPoint.position, 0.25f);
-        DOTween.Kill(obj.transform);
-        Destroy(obj);
+        obj.GetComponent<BoostCardController>().SwitchButtons(false);
+        StartCoroutine(ChangeCardWithAnimation(obj, actionOperation, actionValue));
 
+        selectedCard.GetComponent<CardController>().AddMark(obj.GetComponent<BoostCardController>().boostMark);
+        return true;
+    }
+
+    private IEnumerator ChangeCardWithAnimation(GameObject obj, char actionOperation, char actionValue){
+        selectedCard.GetComponent<CardController>().SwitchMarks(false);
+        
+        ScaleGameObject(obj, new Vector3(1.5f, 1.5f, 1.5f), 0.05f);
+
+        obj.transform.DOLocalMove(selectedPoint.position, 0.25f);
+
+        yield return new WaitForSecondsRealtime(1f);
+        
+        obj.transform.DORotate(new Vector3(0, 90, 0), 0.25f);
+        
         switch(actionOperation){
             case '+':
                 CardFactory.ChangeNumericCard(selectedCard, selectedCardValue + int.Parse(actionValue.ToString()));
@@ -138,8 +90,12 @@ public class GameController : MonoBehaviour
                 Debug.Log("Error in applyBoost switch (unhandled operation)");
                 break;
         }
-        selectedCard.GetComponent<CardController>().AddMark(obj.GetComponent<BoostCardController>().boostMark);
-        return true;
+
+        yield return new WaitForSecondsRealtime(0.25f);
+
+        selectedCard.GetComponent<CardController>().SwitchMarks(true);
+        DOTween.Kill(obj.transform);
+        Destroy(obj);
     }
 
     private void ScaleGameObject(GameObject obj, Vector3 scale, float duration){
